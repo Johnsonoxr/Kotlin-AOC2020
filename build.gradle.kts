0c9d5325c36aab78f8788b78399ba09ad31ff200
Johnson1 Chang <Johnson1_Chang@asus.com>
2023-12-09 23:31:56 +0800

    Init.

---
+++ build.gradle.kts
@@ -0,0 +1,20 @@
+plugins {
+    kotlin("jvm") version "1.9.20"
+}
+
+sourceSets {
+    main {
+        kotlin.srcDir("src")
+    }
+}
+
+tasks {
+    wrapper {
+        gradleVersion = "8.5"
+    }
+}
+
+dependencies {
+    //  coroutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
+}
---
+++ settings.gradle.kts
@@ -0,0 +1,7 @@
+rootProject.name = "Kotlin-AOC2020"
+
+dependencyResolutionManagement {
+    repositories {
+        mavenCentral()
+    }
+}
