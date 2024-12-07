afd47008913fa5e7b39ed8afb8c43f774c4d2231
Johnson1 Chang <Johnson1_Chang@asus.com>
2024-12-08 00:21:27 +0800

    Day09.

---
+++ src/day09/main.kt
@@ -0,0 +1,57 @@
+package day09
+
+import readInput
+import kotlin.system.measureNanoTime
+
+private const val FOLDER = "day09"
+
+fun main() {
+
+    fun parseNumbers(input: List<String>) = input.map { it.toLong() }
+
+    fun part1(input: List<String>, window: Int = 25): Long {
+        parseNumbers(input).windowed(window + 1).forEach { numbers ->
+            val target = numbers.last()
+            val sources = numbers.dropLast(1)
+            sources.forEachIndexed { index, i ->
+                if (sources.subList(index + 1, sources.size).any { it + i == target }) {
+                    return@forEach
+                }
+            }
+            return target
+        }
+        throw IllegalArgumentException("No encryption weakness found.")
+    }
+
+    fun part2(input: List<String>, window: Int = 25): Long {
+        val invalidNumber = part1(input, window)
+        val numbers = parseNumbers(input)
+        numbers.indices.forEach { index ->
+            (2..numbers.size - index - 2).forEach { window ->
+                val windowedNumbers = numbers.subList(index, index + window)
+                if (windowedNumbers.sum() == invalidNumber) {
+                    return windowedNumbers.max() + windowedNumbers.min()
+                }
+            }
+        }
+        throw IllegalArgumentException("No encryption weakness found.")
+    }
+
+    check(part1(readInput("$FOLDER/test"), 5) == 127L)
+    check(part2(readInput("$FOLDER/test"), 5) == 62L)
+
+    val input = readInput("$FOLDER/input")
+    val part1Result: Long
+    val part1Time = measureNanoTime {
+        part1Result = part1(input)
+    }
+    val part2Result: Long
+    val part2Time = measureNanoTime {
+        part2Result = part2(input)
+    }
+
+    println("Part 1 result: $part1Result")
+    println("Part 2 result: $part2Result")
+    println("Part 1 takes ${part1Time / 1e6f} milliseconds.")
+    println("Part 2 takes ${part2Time / 1e6f} milliseconds.")
+}
--- src/day17/main.kt
+++ src/day17/main.kt
@@ -7,45 +7,71 @@ private const val FOLDER = "day17"
 
 fun main() {
 
-    fun parseNumbers(input: List<String>) = input.map { it.toLong() }
+    data class Cube(val coordinate: List<Int>)
 
-    fun part1(input: List<String>, window: Int = 25): Long {
-        parseNumbers(input).windowed(window + 1).forEach { numbers ->
-            val target = numbers.last()
-            val sources = numbers.dropLast(1)
-            sources.forEachIndexed { index, i ->
-                if (sources.subList(index + 1, sources.size).any { it + i == target }) {
-                    return@forEach
+    fun parseCubes(input: List<String>, dimension: Int): Set<Cube> {
+        return input.mapIndexed { y, line ->
+            line.toList().mapIndexed { x, c ->
+                when (c) {
+                    '#' -> Cube(listOf(x, y) + List(dimension - 2) { 0 })
+                    else -> null
                 }
-            }
-            return target
-        }
-        throw IllegalArgumentException("No encryption weakness found.")
+            }.filterNotNull()
+        }.flatten().toSet()
     }
 
-    fun part2(input: List<String>, window: Int = 25): Long {
-        val invalidNumber = part1(input, window)
-        val numbers = parseNumbers(input)
-        numbers.indices.forEach { index ->
-            (2..numbers.size - index - 2).forEach { window ->
-                val windowedNumbers = numbers.subList(index, index + window)
-                if (windowedNumbers.sum() == invalidNumber) {
-                    return windowedNumbers.max() + windowedNumbers.min()
-                }
-            }
-        }
-        throw IllegalArgumentException("No encryption weakness found.")
+    fun Cube.getNeighborsAndSelf(axis: Int = 0): Set<Cube> {
+        val neighborsAlongAxis = coordinate[axis].let { a -> (a - 1..a + 1).map { Cube(coordinate.toMutableList().apply { set(axis, it) }) } }
+        return when {
+            axis == coordinate.size - 1 -> neighborsAlongAxis
+            else -> neighborsAlongAxis.map { it.getNeighborsAndSelf(axis + 1) }.flatten()
+        }.toSet()
     }
 
-    check(part1(readInput("$FOLDER/test"), 5) == 127L)
-    check(part2(readInput("$FOLDER/test"), 5) == 62L)
+    val cachedNeighbors = mutableMapOf<Cube, Set<Cube>>()
+    fun Cube.getNeighbors(): Set<Cube> {
+        return cachedNeighbors.getOrPut(this) {
+            getNeighborsAndSelf() - this
+        }
+    }
+
+    fun runSimulationWithSixTimes(cubes: Set<Cube>): Int {
+        var activeCubes = cubes
+        repeat(6) {
+            val nextActiveCubes = mutableSetOf<Cube>()
+            val inactiveCubes = activeCubes.map { it.getNeighbors() }.flatten().toSet() - activeCubes
+            activeCubes.forEach { activeCube ->
+                if (activeCube.getNeighbors().count { it in activeCubes } in 2..3) {
+                    nextActiveCubes += activeCube
+                }
+            }
+            inactiveCubes.forEach { inactiveCube ->
+                if (inactiveCube.getNeighbors().count { it in activeCubes } == 3) {
+                    nextActiveCubes += inactiveCube
+                }
+            }
+            activeCubes = nextActiveCubes
+        }
+        return activeCubes.size
+    }
+
+    fun part1(input: List<String>): Int {
+        return runSimulationWithSixTimes(parseCubes(input, 3))
+    }
+
+    fun part2(input: List<String>): Int {
+        return runSimulationWithSixTimes(parseCubes(input, 4))
+    }
+
+    check(part1(readInput("$FOLDER/test")) == 112)
+    check(part2(readInput("$FOLDER/test")) == 848)
 
     val input = readInput("$FOLDER/input")
-    val part1Result: Long
+    val part1Result: Int
     val part1Time = measureNanoTime {
         part1Result = part1(input)
     }
-    val part2Result: Long
+    val part2Result: Int
     val part2Time = measureNanoTime {
         part2Result = part2(input)
     }
